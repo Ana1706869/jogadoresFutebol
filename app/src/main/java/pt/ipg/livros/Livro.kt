61a24276ba11842699f343cd460eaecdb5d950e4
Noel Lopes <noel@ipg.pt>
2023-05-16 10:35:48 +0100

    Adicionada data de publicação à tabela de livros

--- app/src/main/java/pt/ipg/livros/Livro.kt
+++ app/src/main/java/pt/ipg/livros/Livro.kt
@@ -1,11 +1,15 @@
 package pt.ipg.livros
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
+import java.util.Calendar
 
 data class Livro(
     var titulo: String,
     var idCategoria: Long,
     var isbn: String? = null,
+    var dataPublicacao: Calendar? = null,
     var id: Long = -1
 ) {
     fun toContentValues() : ContentValues {
@@ -13,8 +17,36 @@ data class Livro(
 
         valores.put(TabelaLivros.CAMPO_TITULO, titulo)
         valores.put(TabelaLivros.CAMPO_ISBN, isbn)
+        valores.put(TabelaLivros.CAMPO_DATA_PUB, dataPublicacao?.timeInMillis)
         valores.put(TabelaLivros.CAMPO_FK_CATEGORIA, idCategoria)
 
         return valores
+    }
+
+    companion object {
+        fun fromCursor(cursor: Cursor) : Livro {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posTitulo = cursor.getColumnIndex(TabelaLivros.CAMPO_TITULO)
+            val posISBN = cursor.getColumnIndex(TabelaLivros.CAMPO_ISBN)
+            val posDataPub = cursor.getColumnIndex(TabelaLivros.CAMPO_DATA_PUB)
+            val posCategoriaFK = cursor.getColumnIndex(TabelaLivros.CAMPO_FK_CATEGORIA)
+
+            val id = cursor.getLong(posId)
+            val titulo = cursor.getString(posTitulo)
+            val isbn = cursor.getString(posISBN)
+
+            var dataPub: Calendar?
+
+            if (cursor.isNull(posDataPub)) {
+                dataPub = null
+            } else {
+                dataPub = Calendar.getInstance()
+                dataPub.timeInMillis = cursor.getLong(posDataPub)
+            }
+
+            val categoriaId = cursor.getLong(posCategoriaFK)
+
+            return Livro(titulo, categoriaId, isbn, dataPub, id)
+        }
     }
 }
