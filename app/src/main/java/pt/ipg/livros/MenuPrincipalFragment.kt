6d2c6c76886852ee058b61c361c394d167d6a1b4
Noel Lopes <noel@ipg.pt>
2023-05-26 09:01:50 +0100

    Criar um fragmento para mostrar a lista de livros e navegação entre o menu e o mesmo

--- app/src/main/java/pt/ipg/livros/MenuPrincipalFragment.kt
+++ app/src/main/java/pt/ipg/livros/MenuPrincipalFragment.kt
@@ -35,6 +35,10 @@ class MenuPrincipalFragment : Fragment() {
         binding.buttonSobre.setOnClickListener {
             findNavController().navigate(R.id.action_MenuPrincipalFragment_to_SobreFragment)
         }
+
+        binding.buttonLivros.setOnClickListener {
+            findNavController().navigate(R.id.action_MenuPrincipalFragment_to_ListaLivrosFragment)
+        }
     }
 
     override fun onDestroyView() {
