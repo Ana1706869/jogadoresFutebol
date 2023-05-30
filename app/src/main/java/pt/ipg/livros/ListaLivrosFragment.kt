02c42d98b952569ff55f41639b8dca35ebad4dc1
Noel Lopes <noel@ipg.pt>
2023-05-30 09:28:16 +0100

    Iniciar um CursorLoader para ler os dados dos livros em background

--- app/src/main/java/pt/ipg/livros/ListaLivrosFragment.kt
+++ app/src/main/java/pt/ipg/livros/ListaLivrosFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import androidx.recyclerview.widget.LinearLayoutManager
 import pt.ipg.livros.databinding.FragmentListaLivrosBinding
@@ -68,7 +69,13 @@ class ListaLivrosFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
      * @return Return a new Loader instance that is ready to start loading.
      */
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
+        return CursorLoader(
+            requireContext(),
+            LivrosContentProvider.ENDERECO_LIVROS,
+            TabelaLivros.CAMPOS,
+            null, null,
+            TabelaLivros.CAMPO_TITULO
+        )
     }
 
     /**
--- app/src/main/java/pt/ipg/livros/LivrosContentProvider.kt
+++ app/src/main/java/pt/ipg/livros/LivrosContentProvider.kt
@@ -276,13 +276,18 @@ class LivrosContentProvider : ContentProvider() {
     companion object {
         private const val AUTORIDADE = "pt.ipg.livros"
 
-        const val CATEGORIAS = "categorias"
-        const val LIVROS = "livros"
+        private const val CATEGORIAS = "categorias"
+        private const val LIVROS = "livros"
 
         private const val URI_CATEGORIAS = 100
         private const val URI_CATEGORIA_ID = 101
         private const val URI_LIVROS = 200
         private const val URI_LIVRO_ID = 201
+
+        private val ENDERECO_BASE = Uri.parse("content://$AUTORIDADE")
+
+        val ENDERECO_CATEGORIAS = Uri.withAppendedPath(ENDERECO_BASE, CATEGORIAS)
+        val ENDERECO_LIVROS = Uri.withAppendedPath(ENDERECO_BASE, LIVROS)
 
         fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, CATEGORIAS, URI_CATEGORIAS)
