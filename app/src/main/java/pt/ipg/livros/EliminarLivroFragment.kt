18fb30b1b2c35f312904516ca79e2b0787adc1f3
Noel Lopes <noel@ipg.pt>
2023-06-13 09:19:53 +0100

    Adicionada fragmento para eliminar livros e correspondente menu

---
+++ app/src/main/java/pt/ipg/livros/EliminarLivroFragment.kt
@@ -0,0 +1,62 @@
+package pt.ipg.livros
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipg.livros.databinding.FragmentEliminarLivroBinding
+
+class EliminarLivroFragment : Fragment() {
+    private var _binding: FragmentEliminarLivroBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentEliminarLivroBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eliminar
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
+            }
+            R.id.action_cancelar -> {
+                voltaListaLivros()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaListaLivros() {
+        findNavController().navigate(R.id.action_eliminarLivroFragment_to_ListaLivrosFragment)
+    }
+
+    private fun eliminar() {
+    }
+}
