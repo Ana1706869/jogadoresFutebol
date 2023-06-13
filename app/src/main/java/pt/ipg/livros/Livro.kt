b8f1a036082cf6e2a32894c416dbd4e3d5830ed9
Noel Lopes <noel@ipg.pt>
2023-06-13 09:30:40 +0100

    Adicionado argumento (livro) ao fragmento eliminar livro

--- app/src/main/java/pt/ipg/livros/Livro.kt
+++ app/src/main/java/pt/ipg/livros/Livro.kt
@@ -3,6 +3,7 @@ package pt.ipg.livros
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
+import java.io.Serializable
 import java.util.Calendar
 
 data class Livro(
@@ -11,7 +12,7 @@ data class Livro(
     var isbn: String? = null,
     var dataPublicacao: Calendar? = null,
     var id: Long = -1
-) {
+) : Serializable {
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
